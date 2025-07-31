a8cf22064d0d61063e7601129f00f231845cce21
meltjacTechs <ebwire02@gmail.com>
2025-07-31 15:47:19 +0300

    Add Navbar component and update Home page layout

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/home";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,7 +9,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
 
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
   </main>
 }
