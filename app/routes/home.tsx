99d48a487b60d3550c00051eeed05dea80758c59
meltjacTechs <ebwire02@gmail.com>
2025-07-31 16:33:55 +0300

    Add ResumeCard component and display resumes on the home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import {resumes} from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -18,5 +20,13 @@ export default function Home() {
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
     </section>
+
+    {resumes.length > 0 &&(
+    <div className="resumes-section">
+      {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+      ))}
+    </div>
+    )}
   </main>
 }
