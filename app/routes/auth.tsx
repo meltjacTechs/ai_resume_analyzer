d54880037818c2295bf796a0ab4ddc7ee41127bd
meltjacTechs <ebwire02@gmail.com>
2025-07-31 17:15:20 +0300

    Add Auth component and update routing to include authentication page

---
+++ app/routes/auth.tsx
@@ -0,0 +1,11 @@
+export const meta = () => ([
+    { title: 'Resumind | Auth' },
+    {name: 'description', content: 'Log into your account.' },
+])
+
+const Auth = () => {
+    return (
+        <div>Auth</div>
+    )
+}
+export default Auth
