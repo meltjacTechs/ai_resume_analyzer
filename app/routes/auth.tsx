998bd1069d995b8508cdf1846fdbf58f9d5f1e03
meltjacTechs <ebwire02@gmail.com>
2025-07-31 17:22:18 +0300

    Update Auth component layout and styling for improved user experience

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -5,7 +5,16 @@ export const meta = () => ([
 
 const Auth = () => {
     return (
-        <div>Auth</div>
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <h1>Welcome</h1>
+                        <h2>Log In to Continue Your Job Journey.</h2>
+                    </div>
+                </section>
+            </div>
+        </main>
     )
 }
 export default Auth
